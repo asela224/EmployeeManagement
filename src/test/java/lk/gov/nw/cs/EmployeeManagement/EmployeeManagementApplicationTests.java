f080ccbe760386090ab5186d248d97cce26ad842
ASELA NIROSHAN Dharmadasa <asela224@gmail.com>
2023-02-25 17:09:47 +0530

    Test Case Added

--- src/test/java/lk/gov/nw/cs/EmployeeManagement/EmployeeManagementApplicationTests.java
+++ src/test/java/lk/gov/nw/cs/EmployeeManagement/EmployeeManagementApplicationTests.java
@@ -3,11 +3,14 @@ package lk.gov.nw.cs.EmployeeManagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class EmployeeManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(true,true);
 	}
 
 }
